4982e1682acff756cfda6ac2fd47f65a0060b9d7
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-24 19:04:05 +0530

    simple script for echo print demo

--- 02-print.sh
+++ 02-print.sh
@@ -16,6 +16,6 @@ echo Hello World
 # "\e[COLm -> this is to enable the color, COL is one of the  color code
 # "\e[0m -> this is to disable the color
 
-echo -e "\e[31mText in Red Color"
+echo -e "\e[31mText in Red Color\e[0m"
 echo "One more line"
 
