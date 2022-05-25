4f6bbd888dcb6d091371cf06c4af6ecc8906cb7b
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-25 18:35:32 +0530

    Input Special Variables

--- 04-Inputs.sh
+++ 04-Inputs.sh
@@ -3,3 +3,13 @@
 read -p 'Enter your Name: ' name
 echo "Your Name = $name"
 
+# Special Variables
+# $0-$n , $* / $@, $#
+echo Script Name = $0
+echo First Argument = $1
+echo All Arguments = $*
+echo Number of Arguments = $#
+
+
+
+
