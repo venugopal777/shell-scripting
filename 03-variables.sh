dac4e93af8a716494ddf1c48ecfad9cbed34ce53
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-25 16:03:02 +0530

    variables declaration , Dynamic data scenario

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,10 +4,11 @@ a=200
 b=devops
 
 echo ${a}times
-echo $b
+echo $b Training
 
 
 # {} are needed if varaibles is combined with other words without spaces
 
 DATE=2022-03-10
 echo Today date is $DATE
+
