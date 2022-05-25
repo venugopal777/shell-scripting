ae4758df8b820e83c12594073c0822cc3cefa666
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-25 11:28:54 +0530

    simple script for echo print demo

--- 02-print.sh
+++ 02-print.sh
@@ -12,11 +12,12 @@ echo Hello World
 
 # Syantx : echo -e "\e[COLmHello\[0m"
 # -e option is to enable esc seq, without -e colors will not work
-# "" Double Quotes are mandatory for colors to work, otherwise it will not work. Optionally we can use sinlgre quotes also,But preffered way is Double Quotes
+# "" Double Quotes are mandatory for colors to work, otherwise it will not work. Optionally we can use sinlge quotes also,But prefered way is Double Quotes
 # "\e[COLm -> this is to enable the color, COL is one of the  color code
 # "\e[0m -> this is to disable the color
 
 echo -e "\e[31mText in Red Color\e[0m"
+echo -e "\e[33mGopal in Yellow Color\e[0m"
 echo "One more line"
 
 echo -e "Line1\n\nline2"
