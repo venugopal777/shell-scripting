1fd3fabcc9bb9f1476a78bce0715e2a94137c303
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-25 22:57:33 +0530

    Quotes Concept

--- 05-quotes.sh
+++ 05-quotes.sh
@@ -5,5 +5,5 @@ echo "Apples Cost is $100"
 echo "Apples Cost is \$200"
 
 TRAINING=DevOps
-echo 'Training = $ {TRAINING}'
-echo "Training = $ {TRAINING}"
+echo 'Training = ${TRAINING}'
+echo "Training = ${TRAINING}"
