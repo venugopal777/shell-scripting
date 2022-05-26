60eaedbff078aa59166177fff7094a697f828414
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-26 23:18:07 +0530

    Condition script

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,9 +8,21 @@ fi
 
 echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 2
+fi
 
 echo -e "\e[36m Downloading Nginx Content \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 2
+fi
 
 echo -e "\e[36m ICleanup Old Nginx Content and Extract new downloaded archive \e[0m"
 rm -rf /usr/share/nginx/html/*
@@ -20,7 +32,19 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 2
+fi
 
 echo -e "\e[36m Starting Nginx \e[0m"
 systemctl restart nginx
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 2
+fi
 systemctl enable nginx
