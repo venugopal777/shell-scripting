99c3d0fc576f3358c3f8eb7ad83824d319eac3fd
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-26 15:16:12 +0530

    Making Frontend Components

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,4 +1,17 @@
-echo Frontend Component
+#!/bin/bash
+
+yum install nginx -y
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 
-
+systemctl restart nginx
+systemctl enable nginx
