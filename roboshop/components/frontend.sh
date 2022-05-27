6ed2f4e39db48ca9bc26bd0072e5e8084416e488
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-27 20:05:24 +0530

    Roboshop frontend Setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+statCheck() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+}
+
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
   echo You should run your script as sudo or root user
@@ -8,21 +17,11 @@ fi
 
 echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
 
 echo -e "\e[36m Downloading Nginx Content \e[0m"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+StatCheck $?
 
 echo -e "\e[36m ICleanup Old Nginx Content and Extract new downloaded archive \e[0m"
 rm -rf /usr/share/nginx/html/*
@@ -32,19 +31,11 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
 
 echo -e "\e[36m Starting Nginx \e[0m"
 systemctl restart nginx
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+StatCheck $?
 systemctl enable nginx
+
+
