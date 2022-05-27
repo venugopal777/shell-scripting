61378de2e8b0172722d2ee099eb3c8b664f854fb
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-27 17:57:43 +0530

    Functions

---
+++ 08-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Variable : If we assign a name to set of data that is variable
+# Function : If we assign a name to set of commands that is function
+
+# func_name() {
+# commands
+# commands
+# }
+
+# func_name
+
+##bDeclare a function
+
+Print_Message() {
+  echo Hello,
+  echo Good Morning
+  echo Welcome to DevOps Training
+}
+
+Print_Message
+
+## Note, Function has always been declared first and then you call function later in the code
