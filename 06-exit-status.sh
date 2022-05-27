bdc4d57d7264bfc65c939b2cfd4f991a6da6ed45
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-27 17:59:14 +0530

    Functions

---
+++ 06-exit-status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit status is a number, $? is a special variable that holds this value
+# $? is going to hav e the exit status of last executed command.
+
+# Range of exit status is 0-255
+# 0 -> Global Success
+# 1-125 -> Some failure from the command
+# 125+ -> System failure
+
