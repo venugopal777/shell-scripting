7bf061455e0994c1e490a6bcb89bbfa66342eeac
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-27 18:33:54 +0530

    Functions

--- 08-functions.sh
+++ 08-functions.sh
@@ -16,7 +16,7 @@ Print_Message() {
   echo Hello,
   echo Good Morning
   echo Welcome to ${1} Training
-  echo "First Argument in Main Script - $1"
+  echo "First Argument in Main Script = $1"
   echo "value of a = $a"
 }
 
@@ -24,7 +24,7 @@ Print_Message() {
 a=10
 Print_Message DevOps
 
-echo "First Argument in Main Script - $1"
+echo "First Argument in Main Script = $1"
 
 ## Note, Function has always been declared first and then you call function later in the code
 ## Function will have its own set of special variables
