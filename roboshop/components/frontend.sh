b2c1dfac2460dab0e8129dbb773a51cda66815d4
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-28 12:12:01 +0530

    Adding Mongodb Setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-StatCheck() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-  fi
-}
-
-Print () {
-  echo -e "\n ------------- $1 -------------" &>>$LOG_FILE
-  echo -e "\e[36m $1 \e[0m"
-}
-
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ]; then
-  echo You should run your script as sudo or root user
-  exit 1
-fi
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
+source components/common.sh
 
 Print "Installing Nginx"
 yum install nginx -y &>>$LOG_FILE
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-StatCheck() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-  fi
-}
-
-Print () {
-  echo -e "\n ------------- $1 -------------" &>>$LOG_FILE
-  echo -e "\e[36m $1 \e[0m"
-}
-
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ]; then
-  echo You should run your script as sudo or root user
-  exit 1
-fi
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
+source components/common.sh
 
 Print "Setup YUM Repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
