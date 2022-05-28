0732de70d2da9e5ed87e7af2af815484bb7280f5
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-28 16:45:17 +0530

    Adding Mongodb Setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -15,14 +15,14 @@ source components/common.sh
 # systemctl enable catalogue
 
 Print "Configure Yum repos"
-curl -f$SL curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG FILE}
+curl -f$SL curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
 StatCheck $?
 
 Print "Install NodeJS"
-yum install nodejs gcc-c++ -y &>>${LOG FILE}
+yum install nodejs gcc-c++ -y &>>${LOG_FILE}
 StatCheck $?
 
-id ${APP_USER} &>>${LOG FILE}
+id ${APP_USER} &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
   Print "Add Application User"
   StatCheck $?
@@ -34,14 +34,15 @@ curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/
 StatCheck $?
 
 Print "CleanUp Old Content"
-rm -rf /home/${APP_USER}/catalogue &>>${LOG FILE}
+rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
 StatCheck $?
 
 Print "Extract App Content"
-cd /home/${APP_USER}/ &>>${LOG FILE} && unzip -o /tmp/catalogue.zip &>>${LOG FILE} && mv catalogue-main catalogue &>>${LOG FILE}
+cd /home/${APP_USER}/ &>>${LOG_FILE} && unzip -o /tmp/catalogue.zip &>>${LOG FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
 StatCheck $?
 
 Print "Fix App User Permissions"
 chown _R ${APP_USER}:${APP_USER} /home/${APP_USER}
 StatCheck $?
+
 
