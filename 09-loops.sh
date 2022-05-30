9f5109444a9d341a76ba898e20b8b665a25d3392
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-30 23:34:19 +0530

    Loops

--- 09-loops.sh
+++ 09-loops.sh
@@ -3,14 +3,9 @@
 # Loop based on expression , while loop command
 # Loop based on inputs , for loop command
 
-i=10
+i=9
 while [ $i -gt 0 ]; do
   echo Iteration - $1
   i=$(($i-1))
 done
-
-for fruit in apple bananan orange ; do
-  echo Fritu Name = $fruit
-done
-
 
