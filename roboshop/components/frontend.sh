f9a8751367ad117682168d533e8e46436a7c6f71
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-30 23:57:57 +0530

    frontend fix

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -22,7 +22,12 @@ StatCheck $?
 
 
 Print "Update RoboSohp Configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' -e '/user/s/localhost/user.roboshop.internal/' -e '/cart/s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf StatCheck $?
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+for component in catalogue user cart ; do
+  echo -e "Updating $component Configuration"
+ sed -i -e "/${catalogue}/s/localhost/{catalogue}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+ StatCheck $?
+done
 
 Print "Starting Nginx"
 systemctl restart nginx &>>$LOG_FILE && systemctl enable nginx &>>$LOG_FILE
