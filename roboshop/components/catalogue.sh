e8e0320750fe7bb0bca24587e1f960b34a719276
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-30 14:28:37 +0530

    Adding Mongodb Setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,18 +2,6 @@
 
 source components/common.sh
 
-#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#$ cd /home/roboshop
-#$ unzip /tmp/catalogue.zip
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
-
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# systemctl daemon-reload
-# systemctl start catalogue
-# systemctl enable catalogue
-
 Print "Configure Yum repos"
 curl -f$SL curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
 StatCheck $?
@@ -40,6 +28,10 @@ Print "Extract App Content"
 cd /home/${APP_USER}/ &>>${LOG_FILE} && unzip -o /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
 StatCheck $?
 
+Print "Install App Dependencies"
+cd /home/${APP_USER}/catalogue &>>${LOG_FILE} && npm install &>>${LOG_FILE}
+StatCheck $?
+
 Print "Fix App User Permissions"
 chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
 StatCheck $?
@@ -48,8 +40,9 @@ Print "Setup SystemD File"
 sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 StatCheck $?
 
-Print "Restart Catalogue Services"
+Print "Restart Catalogue Service"
 systemctl daemon-reload &>>${LOG_FILE} && systemctl restart catalogue &>>${LOG_FILE} &&
 systemctl enable catalogue &>>${LOG_FILE}
 StatCheck $?
+
 
