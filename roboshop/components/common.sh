49fec086cc4b0685da208a471068dc558a0f3b01
Mohit venu gopal <gopaalpanga@gmial.com>
2022-05-31 14:08:29 +0530

    user

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -60,7 +60,7 @@ chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
 StatCheck $?
 
 Print "Setup SystemD File"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service
+sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv/home/roboshop/${COMPONENT}/systemd.service
 /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
 StatCheck $?
 
