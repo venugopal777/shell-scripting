642979ccd7d0a767d5f287da7be775643504bab5
Mohit venu gopal <gopaalpanga@gmial.com>
2022-06-01 16:06:24 +0530

    MySQL Script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -15,34 +15,17 @@ Print "Start MySQL Service"
 systemctl enable mysqld &>>{LOG_FILE} && systemctl start mysqld &>>{LOG_FILE}
 StatCheck $?
 
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
-
-DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-mysql  --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>>{LOG_FILE}
+if [ $? -ne 0 ]; then
+  Print  "Change Default Root Password"
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
+  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+ mysql  --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>{LOG_FILE}
+ StatCheck $?
+fi
 
 
 # SET PASSWORD FOR 'root'@'localhost' = PASSWORD('MyNewPass');
-## mysql_secure_installation
-#```
-#1. You can check the new password working or not using the following command in MySQL
-#
-#First lets connect to MySQL
-#
-#```bash
-## mysql -uroot -pRoboShop@1
-#```
-#Once after login to MySQL prompt then run this SQL Command.
-#
-#```sql
-#> uninstall plugin validate_password;
-#```
-### **Setup Needed for Application.**
-#
-#As per the architecture diagram, MySQL is needed by
-#
-#- Shipping Service
-#
-#So we need to load that schema into the database, So those applications will detect them and run accordingly.
 #
 #To download schema, Use the following command
 #
