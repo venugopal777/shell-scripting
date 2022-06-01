92dccfd3085992112b60e4021108818a89dcd509
Mohit venu gopal <gopaalpanga@gmial.com>
2022-06-01 15:50:38 +0530

    MySQL Script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -18,7 +18,7 @@ StatCheck $?
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
 
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-mysql -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
+mysql  --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
 
 
 # SET PASSWORD FOR 'root'@'localhost' = PASSWORD('MyNewPass');
