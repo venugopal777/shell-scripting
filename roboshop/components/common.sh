4456f1bb7519aa75b8f034c4e3c077ff94ec1200
Mohit venu gopal <gopaalpanga@gmial.com>
2022-06-01 18:35:49 +0530

    Shipping Script

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -22,56 +22,70 @@ rm -f $LOG_FILE
 
 APP_USER=roboshop
 
-NODEJS() {
-
-Print "Configure Yum repos"
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
-StatCheck $?
-
-Print "Install NodeJS"
-yum install nodejs gcc-c++ -y &>>${LOG_FILE}
-StatCheck $?
-
-id ${APP_USER} &>>${LOG_FILE}
-if [ $? -ne 0 ]; then
-  Print "Add Application User"
-  useradd ${APP_USER} &>>${LOG_FILE}
+APP_SETUP() {
+  id ${APP_USER} &>>${LOG_FILE}
+  if [ $? -ne 0 ]; then
+    Print "Add Application User"
+    useradd ${APP_USER} &>>${LOG_FILE}
+    StatCheck $?
+  fi
+  Print "Download App Component"
+  curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
   StatCheck $?
-fi
 
-Print "Download App Component"
-curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
-StatCheck $?
+  Print "CleanUp Old Content"
+  rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
 
-Print "CleanUp Old Content"
-rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
-StatCheck $?
+  Print "Extract App Content"
+  cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+}
 
-Print "Extract App Content"
-cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOG_FILE}
-StatCheck $?
+SERVICE_SETUP() {
+  Print "Fix App User Permissions"
+  chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
+  StatCheck $?
 
-Print "Install App Dependencies"
-cd /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE} && npm install &>>${LOG_FILE}
-StatCheck $?
+  Print "Setup SystemD File"
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e "s/CARTENDPOINT/cart.roboshop.internal/" -e "s/DBHOST/mysql .roboshop.internal/"
+  /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
+  StatCheck $?
 
-Print "Fix App User Permissions"
-chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
-StatCheck $?
+  Print "Restart ${COMPONENT} Service"
+  systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} &&
+  systemctl enable ${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+}
 
-Print "Setup SystemD File"
-sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
-StatCheck $?
+NODEJS() {
+  Print "Configure Yum repos"
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+  StatCheck $?
 
-Print "Restart ${COMPONENT} Service"
-systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} &&
-systemctl enable ${COMPONENT} &>>${LOG_FILE}
-StatCheck $?
+  Print "Install NodeJS"
+  yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+  StatCheck $?
+
+  APP_SETUP
+
+  Print "Install App Dependencies"
+  cd /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE} && npm install &>>${LOG_FILE}
+  StatCheck $?
 
 }
 
+MAVEN() {
+  Print "Install Maven"
+  yum install maven -y &>>${LOG_FILE}
+  StatCheck $?
 
-# -e "s/REDIS_ENDPOINT/redis.roboshop.internal" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/"
+  APP_SETUP
 
+  Print "Maven Packaging"
+  cd /home/${APP_USER}/${COMPONENT} && mvn clean package &>>${LOG_FILE} && mv target/shipping-1.0.jar shipping.jar &>>${LOG_FILE}
+  StatCheck $?
 
+  SERVICE_SETUP
 
+}
---
+++ roboshop/components/shipping.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source components/common.sh
+COMPONENT=shipping
+## Function
+MAVEN
+
+
