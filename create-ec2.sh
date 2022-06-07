1641b94e22a373be77249fe9550b9adbe5f142b1
Mohit venu gopal <gopaalpanga@gmial.com>
2022-06-07 20:01:55 +0530

    Create EC2 Instance Script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Input Machine Name is needed"
+  exit 1
+fi
+
+COMPONENT=$1
+
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+
 
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+
+#--tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
 
