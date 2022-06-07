a581cba21b60565aa15f7e7c800dafcd4ef15c58
Mohit venu gopal <gopaalpanga@gmial.com>
2022-06-07 19:32:54 +0530

    Create EC2 Instance Script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId') sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo $AMI_ID
 
