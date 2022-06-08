9229fbc9c301d58b1ddce1bcb5cdde0c4dd64897
Mohit venu gopal <gopaalpanga@gmial.com>
2022-06-08 16:57:49 +0530

    Create EC2 Instance Script

--- create-ec2.sh
+++ create-ec2.sh
@@ -9,13 +9,20 @@ COMPONENT=$1
 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=mohit | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
-
+aws ec2 run-instances \
+    --image-id ${AMI_ID} \
+    --instance-type t2.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+     -- security-group-ids ${SGID} \
+     | jq
 
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 #--tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
 
+#MarketType=string,SpotOptions={MaxPrice=string,SpotInstanceType=string,BlockDurationMinutes=integer,ValidUntil=timestamp,InstanceInterruptionBehavior=string}
