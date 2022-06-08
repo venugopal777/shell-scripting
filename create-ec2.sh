96283da128650895b8e2543cf1c5bb2ba4585943
Mohit venu gopal <gopaalpanga@gmial.com>
2022-06-08 18:44:48 +0530

    Create EC2 Instance Script

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,23 +6,37 @@ if [ -z "$1" ]; then
 fi
 
 COMPONENT=$1
+ZONE_ID="Z045761334VFD4T3IHM95"
 
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+                   --image-id ${AMI_ID} \
+                   --instance-type t2.micro \
+                   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+                   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+                   --security-group-ids ${SGID} \
+                    | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+                    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+                    aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=mohit | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
-echo $AMI_ID
-aws ec2 run-instances \
-    --image-id ${AMI_ID} \
-    --instance-type t2.micro \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-    --security-group-ids ${SGID} \
-     | jq
+if [ "$1" == "all" ]; then
+  for component in catalogue
+  COMPONENT=$component
+  create_ec2
+  done
+else
+  create_ec2
+file
 
+# echo $AMI_ID
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 #--tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
 
 #MarketType=string,SpotOptions={MaxPrice=string,SpotInstanceType=string,BlockDurationMinutes=integer,ValidUntil=timestamp,InstanceInterruptionBehavior=string}
+
